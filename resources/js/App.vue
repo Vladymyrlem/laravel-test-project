9bfb674d0adde3c6c63b83e8ebbbaf5aa2b13c33
Vladymyr <vovangud@gmail.com>
2025-06-16 15:58:46 +0300

    Create empty VueJs comment components and script for run vue-component

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+<style>
+#app {
+    margin-left: 1em;
+}
+
+.heading h1 {
+    margin-bottom: 0;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import {createApp} from 'vue';
+import axios from 'axios';
+
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/AnswerForm.vue'
+
+const app = createApp(CommentForm)
+app.mount('#vue-app')
