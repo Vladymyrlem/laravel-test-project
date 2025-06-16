1fd3c06a7e57650595987e4797f8446653e6b7be
Vladymyr <vovangud@gmail.com>
2025-06-16 14:57:06 +0300

    Create full routes group

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\CommentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [CommentsController::class, 'index'])->name('comments.index');
+Route::post('comments', [CommentsController::class, 'store'])->name('comments.store');
+Route::get('comments/create', [CommentsController::class, 'create'])->name('comments.create');
+Route::get('comments/{comment}/edit', [CommentsController::class, 'edit'])->name('comments.edit');
+Route::put('comments/{comment}', [CommentsController::class, 'update'])->name('comments.update');
+Route::post('comments/preview', [CommentsController::class, 'preview']);
+Route::get('comments/{comment}', [CommentsController::class, 'show'])->name('comments.show')->whereNumber('comment');
+
+
